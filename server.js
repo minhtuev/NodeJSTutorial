e54e7d1e48a10f9156ea3074fe3e12aa05a07ac9
Minh Tue Vo <mvo@edx.org>
2014-07-26 11:19:53 -0400

    adding a line

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require("http");
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World\n");
-  response.write("I am Minh Tue.")
+  response.write("I am Minh Tue.\n");
+  response.write("Welcome to my world.\n");
   response.end();
 }).listen(8888);
