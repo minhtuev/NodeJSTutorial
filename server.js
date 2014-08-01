aa6faf886990f65fa48377528ea9555074be0fd7
Minh Tue Vo <mvo@edx.org>
2014-08-01 16:36:58 -0400

    adding router module

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ function execute(someFunction, value) {
   someFunction(value);
 }
 
-function start() {
+function start(route) {
   function onRequest(request, response) {
-	  response.writeHead(200, {"Content-Type": "text/plain"});
+	  var pathname = url.parse(request.url).pathname;
+      console.log("Request for " + pathname + " received.");
+      route(pathname);
+      response.writeHead(200, {"Content-Type": "text/plain"});
 	  response.write("Hello World\n");
 	  response.write("I am Minh Tue.\n");
 	  response.write("Welcome to my world.\n");
